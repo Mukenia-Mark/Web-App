d3ff4710e18b2b993ae69fca4b09f4a174bd7532
Mukenia-Mark <Mark.mukenia@gmail.com>
2021-02-22 03:54:43 +0300

    add names to index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
-window.addEventListener("DOMContentLoaded", function ()  {
-  
+document.getElementById('button').addEventListener("click", clickFunction);
+function clickFunction() {
+  alert ("Are you sure that you want to submit?");
+} 
+var day=()
+var gender=()
+if ( day === monday && gender === female ) {
+  alert("Your Akan name is Adwoa.");
+} else if ( day === tuesday && gender === female) {
+  alert("Your Akan name is Abenaa.")
+} else if ( day === wednesday && gender === female) {
+  alert("Your Akan name is Akua.")
+} else if ( day === thursday && gender === female) {
+  alert("Your Akan name is Yaa.")
+} else if ( day === friday && gender === female) {
+  alert("Your Akan name is Afua.")
+} else if ( day === saturday && gender === female) {
+  alert("Your Akan name is Ama")
+} else if ( day === sunday && gender === female) {
+  alert("Your Akan name is Akosua.")
+} else if ( day === monday && gender === male) {
+  alert("Your Akan name is Kwadowa.")
+}else if ( day === tuesday&& gender === male) {
+  alert("Your Akan name is Kwabena.")
+}else if ( day === wednesday && gender === male) {
+  alert("Your Akan name is Kwaku.")
+}else if ( day === thursday && gender === male) {
+  alert("Your Akan name is Yaw")
+}else if ( day === friday && gender === male) {
+  alert("Your Akan name is Kofi.")
+}else if ( day === saturday && gender === male) {
+  alert("Your Akan name is Kwame.")
+}else ( day === sunday && gender === male) {
+  alert("Your Akan name is Kwasi")
 }
