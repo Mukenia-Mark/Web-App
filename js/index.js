ef49698064af6f72df60a8d753b1c52799b9ee60
Mukenia-Mark <Mark.mukenia@gmail.com>
2021-02-22 04:54:13 +0300

    add function to show date and gender

--- js/index.js
+++ js/index.js
@@ -1,35 +1,10 @@
 document.getElementById('button').addEventListener("click", clickFunction);
 function clickFunction() {
-  alert ("Are you sure that you want to submit?");
+alert ("Are you sure that you want to submit?");
 } 
-var day=()
-var gender=()
-if ( day === monday && gender === female ) {
-  alert("Your Akan name is Adwoa.");
-} else if ( day === tuesday && gender === female) {
-  alert("Your Akan name is Abenaa.")
-} else if ( day === wednesday && gender === female) {
-  alert("Your Akan name is Akua.")
-} else if ( day === thursday && gender === female) {
-  alert("Your Akan name is Yaa.")
-} else if ( day === friday && gender === female) {
-  alert("Your Akan name is Afua.")
-} else if ( day === saturday && gender === female) {
-  alert("Your Akan name is Ama")
-} else if ( day === sunday && gender === female) {
-  alert("Your Akan name is Akosua.")
-} else if ( day === monday && gender === male) {
-  alert("Your Akan name is Kwadowa.")
-}else if ( day === tuesday&& gender === male) {
-  alert("Your Akan name is Kwabena.")
-}else if ( day === wednesday && gender === male) {
-  alert("Your Akan name is Kwaku.")
-}else if ( day === thursday && gender === male) {
-  alert("Your Akan name is Yaw")
-}else if ( day === friday && gender === male) {
-  alert("Your Akan name is Kofi.")
-}else if ( day === saturday && gender === male) {
-  alert("Your Akan name is Kwame.")
-}else ( day === sunday && gender === male) {
-  alert("Your Akan name is Kwasi")
+document.forms["akanForm"].onsubmit = function(){
+  var day = document.getElementById("dateofbirth").value;
+  var gender = document.getElementById("gender").value;
+  alert( " " day + "and " + gender);
+  return false;
 }
