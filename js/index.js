8aa18295bbc71b5995a028d4f89a1963ffe611d4
Mukenia-Mark <Mark.mukenia@gmail.com>
2021-02-22 08:24:57 +0300

    correct spelling mistakes

--- js/index.js
+++ js/index.js
@@ -2,42 +2,43 @@ document.getElementById('button').addEventListener("click", clickFunction);
 function clickFunction() {
 alert ("Are you sure that you want to submit?");
 } 
+
 var dateinput=document.getElementById("dateofbirth").value;
 var dateofbirth= new Date(dateinput);
 var day = dateofbirth.getDay();
-alert("The day of the week is "+day+".");
+alert("The day of the week is "+dateofbirth+".");
 var gender = document.getElementById("gender").value;
 alert("You are "+gender+".");
 
 
-if ( day === 1 && gender === "Female" ) {
+if ( day === "1" && gender === "Female" ) {
 alert("Your Akan name is Adwoa.");
-} else if ( day === 2 && gender === "Female") {
+} else if ( day === "2" && gender === "Female") {
 alert("Your Akan name is Abenaa.");
-} else if ( day === 3 && gender === "Female") {
+} else if ( day === "3" && gender === "Female") {
 alert("Your Akan name is Akua.");
-} else if ( day === 4 && gender === "Female") {
+} else if ( day === "4" && gender === "Female") {
 alert("Your Akan name is Yaa.");
-} else if ( day === 5 && gender === "Female") {
+} else if ( day === "5" && gender === "Female") {
 alert("Your Akan name is Afua.");
-} else if ( day === 6 && gender === "Female") {
+} else if ( day === "6" && gender === "Female") {
 alert("Your Akan name is Ama");
-} else if ( day === 0 && gender === "Female") {
+} else if ( day === "0" && gender === "Female") {
 alert("Your Akan name is Akosua.");
-} else if ( day === 1 && gender === "Male") {
+} else if ( day === "1" && gender === "Male") {
 alert("Your Akan name is Kwadowa.");
-}else if ( day === 2 && gender === "Male") {
+}else if ( day === "2" && gender === "Male") {
 alert("Your Akan name is Kwabena.");
-}else if ( day === 3 && gender === "Male") {
+}else if ( day === "3" && gender === "Male") {
 alert("Your Akan name is Kwaku.");
-}else if ( day === 4 && gender === "Male") {
+}else if ( day === "4" && gender === "Male") {
 alert("Your Akan name is Yaw");
-}else if ( day === 5 && gender === "Male") {
+}else if ( day === "5" && gender === "Male") {
 alert("Your Akan name is Kofi.");
-}else if ( day === 6 && gender === "Male") {
+}else if ( day === "6" && gender === "Male") {
 alert("Your Akan name is Kwame.");
-}else if ( day === 0 && gender === "Male") {
+}else if ( day === "0" && gender === "Male") {
 alert("Your Akan name is Kwasi")
 }else {
-alert("Your imput is incorect.");
+alert("Your input is incorrect.");
 }
